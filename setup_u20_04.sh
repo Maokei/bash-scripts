f12c79ad407f3a2391d202c021c98206c758a3dd
maokei <rickard.joh@gmail.com>
2020-05-06 11:47:31 +0200

    20.04

--- setup_u20_04.sh
+++ setup_u20_04.sh
@@ -34,7 +34,7 @@ sudo apt update
 
 #Docker
 curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+#sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 
 #Brave brower
 curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
