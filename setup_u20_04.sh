d8156a039e02f4a8f90c33a5eceeb29774a08fad
Rickard <rickard.joh@gmail.com>
2020-05-18 12:42:47 +0200

    Update setup_u20_04.sh
    Add yarn global to PATH

--- setup_u20_04.sh
+++ setup_u20_04.sh
@@ -97,6 +97,7 @@ sudo -u $USER echo "fi" >> ~/.bashrc
 
 sudo -u $USER mkdir ~/.yarn-global
 sudo -u $USER yarn config set globa-dir ~/.yarn-global
+sudo -u $USER echo 'export PATH="$PATH:$(yarn global bin)"' >> ~/.bash_profile
 
 sudo -u $USER mkdir ~/.npm-global
 sudo -u $USER npm config set prefix '~/.npm-global'
