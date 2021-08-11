c7108363e1493681e03fd4c717d9f41f29b068ee
Rickard <rickard.joh@gmail.com>
2021-08-11 20:31:50 +0200

    Create setup_manjaro.sh

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -12,24 +12,25 @@ sudo snap install code --classic
 sudo snap install microk8s --classic
 sudo snap install insomnia
 
-sudo usermod -aG microk8s ${USER} 
+sudo usermod -aG microk8s ${USER}
 
+sudo -u $USER echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
 sudo -u $USER nvm install 14
-sudo -u $USER npm install --global yarn
+
+# Npm
+sudo -u $USER mkdir ~/.npm-global
+sudo -u $USER npm config set prefix '~/.npm-global'
+sudo -u $USER echo 'PATH=~/.npm-global/bin:$PATH' >> ~/.zshenv
+source ~/.zshenv
+sudo -u $USER npm install -g yarn
 
 # Yarn
 sudo -u $USER mkdir ~/.yarn-global
 sudo -u $USER yarn config set global-folder ~/.yarn-global
 sudo -u $USER yarn config set prefix ~/.yarn-global
-sudo -u $USER echo 'export PATH="$PATH:$(yarn global bin)"' >> ~/.bash_profile
-# Npm
-sudo -u $USER mkdir ~/.npm-global
-sudo -u $USER npm config set prefix '~/.npm-global'
-sudo -u $USER echo 'PATH=~/.npm-global/bin:$PATH' >> ~/.bash_profile
-source ~/.bash_profile
+sudo -u $USER echo 'export PATH="$PATH:$(yarn global bin)"' >> ~/.zshenv
 
 sudo -u $USER pip3 install --user neovim 
 
 sudo -u $USER echo "export DOTNET_CLI_TELEMETRY_OPTOUT=1" >> ~/.zshrc
 sudo -u $USER curl -s 'https://get.sdkman.io' | bash
-
