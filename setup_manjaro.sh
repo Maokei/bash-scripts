0abd99b2c4521b501b859b1dfd0222ce51744b76
maokei <rickard.joh@gmail.com>
2021-08-25 14:38:19 +0200

    arch things

--- setup_manjaro.sh
+++ setup_manjaro.sh
@@ -1,6 +1,40 @@
 #!/usr/bin/env bash
-sudo pacman -Syu
-sudo pacman -S base-devel neofetch docker ruby nvm tlp
+sudo pacman -Syu --noconfirm
+sudo pacman -S --noconfirm base-devel neofetch docker ruby tlp git neovim python python-pip
+
+get_distro_name() {
+	local distro=$(awk  -F= '$1=="ID" { print $2 ;}' /etc/os-release)
+	echo "$distro"
+}
+
+distro=$(get_distro_name)
+if [ "$distro" == "arch" ]; then
+	echo "Doing arch things"
+	
+	sudo -u $USER git clone https://aur.archlinux.org/yay-git.git
+	cd yay-git
+	sudo -u $USER makepkg -si --noconfirm
+	cd ..
+	rm -rf yay-git
+	
+	sudo -u $USER git clone https://aur.archlinux.org/nvm.git
+	cd nvm
+	sudo -u $USER makepkg -si --noconfirm
+	rm -rf nvm
+	
+	sudo pacman -S --noconfirm zsh zsh-completions zsh-theme-powerlevel10k
+	sudo -u $USER chsh -s /bin/zsh
+	sudo -u $USER echo 'source /usr/share/zsh-theme-powerlevel10k/powerlevel10k.zsh-theme' >>! ~/.zshrc
+	
+	sudo -u $USER git clone https://aur.archlinux.org/snapd.git
+	cd snapd
+	sudo -u $USER makepkg -si --noconfirm
+	cd ..
+	rm -rf snapd
+	sudo systemctl enable --now snapd.socket
+	sudo ln -s /var/lib/snapd/snap /snap
+
+fi 
 
 sudo usermod -aG docker ${USER}
 sudo systemctl enable docker.service
@@ -15,13 +49,15 @@ sudo snap install insomnia
 sudo usermod -aG microk8s ${USER}
 
 sudo -u $USER echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.zshrc
+sudo -u $USER echo 'source /usr/share/nvm/init-nvm.sh' >> ~/.bashrc
 sudo -u $USER nvm install 14
 
 # Npm
 sudo -u $USER mkdir ~/.npm-global
 sudo -u $USER npm config set prefix '~/.npm-global'
 sudo -u $USER echo 'PATH=~/.npm-global/bin:$PATH' >> ~/.zshenv
-source ~/.zshenv
+sudo -u $USER echo 'PATH=~/.npm-global/bin:$PATH' >> ~/.bash_profile
+source ~/.bash_profile
 sudo -u $USER npm install -g yarn
 
 # Yarn
@@ -29,8 +65,13 @@ sudo -u $USER mkdir ~/.yarn-global
 sudo -u $USER yarn config set global-folder ~/.yarn-global
 sudo -u $USER yarn config set prefix ~/.yarn-global
 sudo -u $USER echo 'export PATH="$PATH:$(yarn global bin)"' >> ~/.zshenv
+sudo -u $USER echo 'export PATH="$PATH:$(yarn global bin)"' >> ~/.bash_profile
 
 sudo -u $USER pip3 install --user neovim 
 
 sudo -u $USER echo "export DOTNET_CLI_TELEMETRY_OPTOUT=1" >> ~/.zshrc
+sudo -u $USER echo "export DOTNET_CLI_TELEMETRY_OPTOUT=1" >> ~/.bashrc
 sudo -u $USER curl -s 'https://get.sdkman.io' | bash
+
+echo "autoload -Uz zsh-newuser-install"
+echo "zsh-newuser-install -f"
