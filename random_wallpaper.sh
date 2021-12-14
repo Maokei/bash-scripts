0ef0130711935d76415cef095c69827df854497c
maokei <rickard.joh@gmail.com>
2021-12-14 22:41:24 +0100

    random wallpapper using find

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -8,22 +8,15 @@
 export wallpaper_path="/home/$USER/Pictures"
 shopt -s nullglob
 # store all the image file names in wallpapers array
-wallpapers=(
-    $wallpaper_path/*.jpg
-    $wallpaper_path/*.jpeg
-    $wallpaper_path/*.png
-    $wallpaper_path/*.bmp
-    $wallpaper_path/*.svg
-)
-# get array size
-# wallpapers_size=${#wallpapers[*]}
+IFS=$'\n'
+wallpapers=($(find $wallpaper_path -iregex '.*\.\(jpg\|gif\|png\|jpeg\)$'))
+unset IFS
 
 # random from array
 while true
 do
     rand=$[$RANDOM % ${#wallpapers[@]}]
     file=${wallpapers[$rand]}
-    FILE_PATH=$( echo "$file" | sed 's/ /\\ /g' )
-    gsettings set org.gnome.desktop.background picture-uri ${FILE_PATH}
+    gsettings set org.gnome.desktop.background picture-uri "${file}"
     sleep 15m
 done
