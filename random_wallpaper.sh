a0c9bd9d9af5662f45a8a69f0dd4fbd71dfed089
maokei <rickard.joh@gmail.com>
2021-12-14 19:48:40 +0100

    random wallpaper crap

---
+++ random_wallpaper.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# start my custom script for setting random background wallpapers
+#if [ -f "$HOME/.random_wallpapers.sh" ] ; then
+#    bash $HOME/.random_wallpapers.sh &
+#fi
+
+export wallpaper_path="/home/$USER/Pictures"
+shopt -s nullglob
+# store all the image file names in wallpapers array
+wallpapers=(
+    $wallpaper_path/*.jpg
+    $wallpaper_path/*.jpeg
+    $wallpaper_path/*.png
+    $wallpaper_path/*.bmp
+    $wallpaper_path/*.svg
+)
+echo "$wallpapers"
+# get array size
+# wallpapers_size=${#wallpapers[*]}
+
+# random from array
+while true
+do
+    rand=$[$RANDOM % ${#wallpapers[@]}]
+    file=${wallpapers[$rand]}
+    FILE_PATH=$( echo "$file" | sed 's/ /\\ /g' )
+    gsettings set org.gnome.desktop.background picture-uri ${FILE_PATH}
+    sleep 15m
+done
