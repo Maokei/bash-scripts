4e5bbc1a7df77428fee6fa846a740a2e3f64a473
maokei <rickard.joh@gmail.com>
2021-12-21 01:52:10 +0100

    systemd example

--- random_wallpaper.sh
+++ random_wallpaper.sh
@@ -1,22 +1,40 @@
 #!/bin/bash
 
-# start my custom script for setting random background wallpapers
-#if [ -f "$HOME/.random_wallpapers.sh" ] ; then
-#    bash $HOME/.random_wallpapers.sh &
-#fi
+: '
+Assuming executable script
+/usr/lib/systemd/user/wallpaper.service
 
-export wallpaper_path="/home/$USER/Pictures"
+[Unit]
+Description=Random wallpaper
+
+[Service]
+ExecStart=/usr/bin/random_wallpaper.sh
+Type=oneshot
+RemainAfterExit=yes
+
+[Install]
+WantedBy=multi-user.target
+'
+
+export wallpapers_path="/home/$USER/Pictures"
 shopt -s nullglob
 # store all the image file names in wallpapers array
 IFS=$'\n'
-wallpapers=($(find $wallpaper_path -iregex '.*\.\(jpg\|gif\|png\|jpeg\)$'))
+wallpapers=($(find $wallpapers_path -iregex '.*\.\(jpg\|gif\|png\|jpeg\)$'))
 unset IFS
+
+setWallpaper() {
+    echo "Setting wallpaper: $1"
+    if [ "$DESKTOP_SESSION" == "gnome" ]; then   
+        gsettings set org.gnome.desktop.background picture-uri "${file}"
+    fi
+}
 
 # random from array
 while true
 do
     rand=$[$RANDOM % ${#wallpapers[@]}]
     file=${wallpapers[$rand]}
-    gsettings set org.gnome.desktop.background picture-uri "${file}"
+    setWallpaper $file
     sleep 15m
 done
