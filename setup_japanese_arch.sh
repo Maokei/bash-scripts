8596d1da754fe22f90e69ce9f26689ea403cafde
maokei <rickard.joh@gmail.com>
2022-02-19 20:17:43 +0100

    jp

--- setup_japanese_arch.sh
+++ setup_japanese_arch.sh
@@ -1,2 +1,24 @@
 #!/usr/bin/env bash
-echo "setup todo"
+echo "Installing packages for Japanese input"
+
+install_fcitx() {
+  echo "installing fcitx"
+  echo "GTK_IM_MODULE=fcitx" >> ~/.profile
+  echo "QT_IM_MODULE=fcitx" >> ~/.profile
+  echo "XMODIFIERS=@im=fcitx" >> ~/.profile
+  sudo pacman -S --noconfirm fcitx5 fcitx5-configtool \
+  fcitx5-gtk fcitx5-mozc fcitx5-qt
+}
+
+install_ibus() {
+  echo "installing ibus"
+  #sudo pacman -S ibus ibus-anthy
+}
+
+install_fonts() {
+  echo "installing fonts"
+  sudo pacman -S --noconfirm otf-ipafont adobe-source-han-sans-jp-fonts adobe-source-han-serif-jp-fonts ttf-hanazono
+}
+
+install_fcitx
+install_fonts
