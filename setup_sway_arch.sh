25396e9abd336603cd152c4b0abb03c0b41ed2d0
maokei <rickard.joh@gmail.com>
2022-04-09 19:20:54 +0200

    sway

--- setup_sway_arch.sh
+++ setup_sway_arch.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 echo "Sway setup"
 install_packages() {
-  sudo pacman -S --no-confirm sway i3status swayidle swaylock alacritty
+  sudo pacman -S --noconfirm sway i3status swayidle swaylock waybar otf-font-awesome alacritty bemenu bemenu-wayland mako grim udiskie pavucontrol blueman
 }
